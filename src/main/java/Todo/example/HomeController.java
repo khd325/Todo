109865024939f3cebf148f65b615054617de16ce
khd325 <hdk325@naver.com>
2022-12-16 14:07:17 +0900

    Home Controller 구현

---
+++ src/main/java/Todo/example/HomeController.java
@@ -0,0 +1,13 @@
+package Todo.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String homeController() {
+        return "To-do Application !";
+    }
+}
